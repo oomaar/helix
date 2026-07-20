433bae8a91fc6b8ddcc117839f18e8fa80d61c96
oomaar <omar16694@gmail.com>
2026-07-20 12:57:57 +0300

    feat: introduce SparkLine component

---
+++ src/shared/charts/sparkline/index.ts
@@ -0,0 +1,2 @@
+export { Sparkline } from "./sparkline";
+export type { SparklineProps } from "./types/sparkline.types";
---
+++ src/shared/charts/sparkline/types/sparkline.types.ts
@@ -0,0 +1,8 @@
+export type SparklineProps = {
+  values: readonly number[];
+  width?: number;
+  height?: number;
+  color?: string;
+  fill?: boolean;
+  strokeWidth?: number;
+};
